d54d43ead158a7c2c0bae6fb40b3f5ac9d07684c
Nick Stacey <nick@ditt.to>
2016-11-02 18:49:12 +0000

    Adds basic menu setup

---
+++ Assets/LevelSelectControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectControl : MonoBehaviour
+{
+    public GameObject levelSelect;
+
+    public GameObject loadingScreen;
+
+    public void ShowLevelControl()
+    {
+        StartCoroutine(FadeIn(levelSelect));
+    }
+
+    public void SelectLevel(int sceneNumber)
+    {
+        // fade in loading screen
+        StartCoroutine(FadeIn(loadingScreen));
+
+        // load level async
+        StartCoroutine(LoadScene(sceneNumber));
+    }
+
+    public IEnumerator FadeIn(GameObject obj)
+    {
+        CanvasGroup canvas = obj.GetComponent<CanvasGroup>();
+
+        canvas.alpha = 0;
+        obj.SetActive(true);
+
+        for (int i = 0; i < 100; i = i + 2) {
+            canvas.alpha = i / 100f;
+            yield return null;
+        }
+    }
+
+    private IEnumerator LoadScene(int sceneId)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneId);
+
+        async.allowSceneActivation = false;
+        yield return new WaitForSeconds(3f);
+        async.allowSceneActivation = true;
+
+        while (!async.isDone) {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/SplashScreenControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LevelSelectControl))]
+public class SplashScreenControl : MonoBehaviour
+{
+    public GameObject splashScreen;
+
+    public void Start()
+    {
+        splashScreen.SetActive(true);
+        StartCoroutine(CoroutineShowSplashScreen());
+    }
+
+    private IEnumerator CoroutineShowSplashScreen()
+    {
+        yield return new WaitForSeconds(3f);
+
+        CanvasGroup canvas = splashScreen.GetComponent<CanvasGroup>();
+
+        for (int i = 100; i > 0; i = i - 2) {
+            canvas.alpha = i / 100f;
+            yield return null;
+        }
+
+        splashScreen.SetActive(false);
+
+        this.GetComponent<LevelSelectControl>().ShowLevelControl();
+    }
+}
